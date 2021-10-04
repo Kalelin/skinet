c8b8c5990cda04d274253e9f9f930ab8369c7ebb
e.kalugina <e.kalugina@andersenlab.com>
2021-10-04 13:18:18 +0300

    Section 3. Added seed data and repository

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Interfaces
+{
+    using Core.Entities;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+
+        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
+    }
+}
