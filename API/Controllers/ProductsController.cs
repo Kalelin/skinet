f99e1165fb5020ea1bf9da3b10e6d3b216e84a80
e.kalugina <elina_kalugina@mail.ru>
2021-10-13 13:19:49 +0300

    Section 6. Added Filtering, Pagination, Sorting and Name Search

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 {
     using API.DTO;
     using API.Errors;
+    using API.Helpers;
     using AutoMapper;
     using Core.Entities;
     using Core.Interfaces;
@@ -32,13 +33,18 @@
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductDTO>>> GetProducts()
+        public async Task<ActionResult<Pagination<ProductDTO>>> GetProducts([FromQuery]ProductSpecParams productParams)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
+            var countSpec = new ProductWithFiltersForCountSpecification(productParams);
+
+            var totalItemsCount = await _productRepo.CountAsync(countSpec);
 
             var products = await _productRepo.ListAsync(spec);
 
-            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductDTO>>(products));
+            var data = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductDTO>>(products);
+
+            return Ok(new Pagination<ProductDTO>(productParams.PageIndex, productParams.PageSize, totalItemsCount, data));
         }
 
         [HttpGet("{id}")]
