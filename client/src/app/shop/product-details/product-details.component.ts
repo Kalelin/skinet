2be5733e702bd6934c1009716411eea77a60d909
e.kalugina <elina_kalugina@mail.ru>
2021-11-12 13:38:26 +0300

    Section 12. Added styling

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -11,17 +12,17 @@ import { ShopService } from '../shop.service';
 export class ProductDetailsComponent implements OnInit {
   product!: IProduct;
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { 
-    this.loadProduct();
-  }
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) { }
 
   ngOnInit(): void {
+    this.loadProduct();
   }
 
   loadProduct(){
     let id: string = this.activatedRoute.snapshot.paramMap.get('id') ?? "0";
     this.shopService.getProduct(+id).subscribe((product: IProduct) => {
       this.product = product;
+      this.bcService.set('@productDetails', product.name)
     }, (error: any) => {
       console.log(error);
     });
